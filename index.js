6bf03c1c7ad68d4ee4a11c2158550fb83bd12703
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2020-03-19 14:49:26 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict";
+const Hapi = require("@hapi/hapi");
+const parseData = require("./parser");
+const axios = require("axios");
+const url = "https://www.worldometers.info/coronavirus/#countries";
+
+const init = async () => {
+	const server = Hapi.server({
+		port: 3000,
+		host: "localhost"
+	});
+
+	server.route({
+		method: "GET",
+		path: "/covid-19.log",
+		handler: (request, h) => {
+			const country = request.query.country;
+			return axios.get(url).then(response => {
+				return parseData(response, country, "log");
+			});
+		}
+	});
+
+	server.route({
+		method: "GET",
+		path: "/covid-19.json",
+		handler: (request, h) => {
+			return axios.get(url).then(response => {
+				const country = request.query.country;
+				return parseData(response, country, "json");
+			});
+		}
+	});
+
+	await server.start();
+	console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", err => {
+	console.log(err);
+	process.exit(1);
+});
+
+init();
---
+++ parser.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const cheerio = require("cheerio");
+
+function getValue(element, selector) {
+	return cheerio
+		.load("")(element)
+		.find(selector)
+		.text()
+		.replace(/\s/g, "");
+}
+
+function parseData(response, country, type) {
+	const $ = cheerio.load(response.data.toString());
+	const datatime = new Date()
+		.toJSON()
+		.slice(0, 19)
+		.replace(/[-T]/g, ":");
+	let state = {};
+
+	$("#main_table_countries_today > tbody")
+		.children()
+		.each((i, tr) => {
+			if (
+				$(tr)
+					.find("td:nth-child(1)")
+					.text()
+					.replace(" ", "") === country
+			) {
+				state = {
+					country: getValue(tr, "td:nth-child(1)"),
+					cases: {
+						total: getValue(tr, "td:nth-child(2)"),
+						new:
+							getValue(tr, "td:nth-child(3)") !== ""
+								? getValue(tr, "td:nth-child(3)")
+								: "0",
+						active: getValue(tr, "td:nth-child(7)")
+					},
+					deaths: {
+						total: getValue(tr, "td:nth-child(4)"),
+						today:
+							getValue(tr, "td:nth-child(5)") !== ""
+								? getValue(tr, "td:nth-child(5)")
+								: "0"
+					},
+					recovered: getValue(tr, "td:nth-child(6)")
+				};
+			}
+		});
+
+	if (type === "json") {
+		return state;
+	} else {
+		return `${state.cases.total} ${state.cases.new} ${state.deaths.total} ${state.recovered}`;
+	}
+}
+module.exports = parseData;
+// fs.writeFileSync("./covid.log", log, { encoding: "utf8", flag: "w" });
